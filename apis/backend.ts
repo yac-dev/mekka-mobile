8be523ff3abae1b829a7c1bdf636b25de0bc0686
kojima yosuke <yabbee0828@gmail.com>
2023-11-20 17:01:02 +0900

    Fix for mekkadevelopment build

--- apis/backend.ts
+++ apis/backend.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import baseURL from './baseURL';
+import Constants from 'expo-constants';
+// 基本、APP_VARIANTに依って使うapiを変える感じだ。
+const endpoint = Constants.expoConfig.extra.apiEndpoint;
 
 // login後、動かね〜。。。
 const backendAPI = axios.create({
-  baseURL: 'http://192.168.11.3:3500/api', // local
+  baseURL: Constants.expoConfig.extra.apiEndpoint, // local
   // baseURL: 'http://10.113.214.33:3500/api', // local
   // baseURL: 'http://localhost:3500/api', // local
   // baseURL: 'https://mekka-backend-staging.onrender.com' // staging
--- apis/baseURL.ts
+++ apis/baseURL.ts
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
+import Constants from 'expo-constants';
+// 基本、APP_VARIANTに依って使うapiを変える感じだ。
+const endpoint = Constants.expoConfig.extra.apiEndpoint;
+console.log(endpoint);
+
 // const baseURL = 'http://192.168.11.30:3500'; // local
-const baseURL = 'http://192.168.11.3:3500'; // local
+const baseURL = endpoint; // local
 // const baseURL = 'https://mekka-backend-staging.onrender.com'; // staging
 // const baseURL = 'http://localhost:3500'; // local
+
+console.log('base url is ', baseURL);
 export default baseURL;
