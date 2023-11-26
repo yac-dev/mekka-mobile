bb8785d7bcc0df9c63dddae9fca5f171a3ebf819
kojima yosuke <yabbee0828@gmail.com>
2023-11-26 22:56:39 +0900

    Prebuild

--- apis/backend.ts
+++ apis/backend.ts
@@ -7,10 +7,11 @@ import Constants from 'expo-constants';
 // login後、動かね〜。。。
 const backendAPI = axios.create({
   // baseURL: Constants.expoConfig.extra.apiEndpoint, // local
-  // baseURL: 'http://192.168.11.4:3500/api', // local
+  baseURL: 'http://192.168.11.4:3500/api', // local
   // baseURL: 'http://10.113.214.33:3500/api', // local
   // baseURL: 'https://mekka-backend-staging.onrender.com/api' // staging
-  baseURL: 'https://mekka-production-server.onrender.com/api', // production
+  // baseURL: 'https://dull-tan-barnacle-cuff.cyclic.app/api', // production // cyclic version
+  // baseURL: 'https://web-production-8152.up.railway.app/api', // production // railway version
 });
 
 export default backendAPI;
