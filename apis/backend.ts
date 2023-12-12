a68802d51f3b311fd5e80c8083998dd5b2c424e2
kojima yosuke <yabbee0828@gmail.com>
2023-12-13 00:04:04 +0900

    Set up info plist, environment etc

--- apis/backend.ts
+++ apis/backend.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import baseURL from './baseURL';
-import Constants from 'expo-constants';
+// import Constants from 'expo-constants';
 // 基本、APP_VARIANTに依って使うapiを変える感じだ。
 // const endpoint = Constants.expoConfig.extra.apiEndpoint;
+import Config from 'react-native-config';
 
 // login後、動かね〜。。。
 const backendAPI = axios.create({
+  baseURL: Config.API_ENDPOINT,
   // baseURL: Constants.expoConfig.extra.apiEndpoint, // local
-  baseURL: 'http://192.168.11.4:3500/api', // local
+  // baseURL: 'http://192.168.11.4:3500/api', // local
   // baseURL: 'https://mekka-staging-955e17db8bae.herokuapp.com/api', // staging // heroku version
   // baseURL: 'https://mekka-production-1ec261d2f6ac.herokuapp.com/api', // production // heroku version
 
