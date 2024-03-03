579d9e9e716ae2a9638623c3ec40e39311db3bea
kojima yosuke <yabbee0828@gmail.com>
2024-03-03 23:40:25 +0900

    Impl forgot password page

---
+++ features/Login/hooks/useEmailForm.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+type EmailFormType = {
+  value: string;
+  isValidated: boolean;
+};
+
+type UseEmailFormOutput = {
+  emailForm: EmailFormType;
+  onEmailChange: (text: string) => void;
+};
+
+export const useEmailForm = (): UseEmailFormOutput => {
+  const [emailForm, setEmailForm] = useState<EmailFormType>({
+    value: '',
+    isValidated: false,
+  });
+
+  const onEmailChange = (text: string) => {
+    setEmailForm((previous) => {
+      return {
+        ...previous,
+        email: {
+          value: text,
+          isValidated: text.length ? true : false,
+        },
+      };
+    });
+  };
+
+  return {
+    emailForm,
+    onEmailChange,
+  };
+};
--- features/Login/pages/ForgotPassword.tsx
+++ features/Login/pages/ForgotPassword.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { PageScreen } from '../../../components';
+import { AppTextInput } from '../../../components';
+import { useEmailForm } from '../hooks';
+import { VectorIcon } from '../../../Icons';
 
 // ここでemailを入力させて、emailに送る。そんで、indicator出して、pageをenter pin pageへnavigateする。
 export const ForgotPassword = () => {
+  const { emailForm, onEmailChange } = useEmailForm();
   return (
-    <PageScreen.WithTitle title='Reset password' subTitle='forgot??'>
-      <View>
-        <Text>Forgot my password</Text>
+    <PageScreen.WithTitle
+      title='Reset password'
+      subTitle={`We'll send you a verification code to this email${'\n'}if it matches an existing Mekka account.`}
+    >
+      <View style={{ paddingHorizontal: 10 }}>
+        <AppTextInput.Underline
+          placeholder='Email'
+          value={emailForm.value}
+          onTextChange={onEmailChange}
+          labelIcon={<VectorIcon.MCI name='email' color={'white'} size={25} />}
+          keyboardType='email-address'
+        />
       </View>
     </PageScreen.WithTitle>
   );
