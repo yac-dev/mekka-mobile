ce0c7a43d31aa1f51f5b34b400accc7a3fa30012
kojima yosuke <yabbee0828@gmail.com>
2024-05-13 23:32:05 +0900

    Fix login behavior

--- features/WelcomePage/pages/WelcomePage.tsx
+++ features/WelcomePage/pages/WelcomePage.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
@@ -9,11 +9,33 @@ import { RootStackNavigatorProps } from '../../App';
 import { AppTextInput } from '../../../components/AppTextInput';
 import { useForm } from '../../Login/hooks';
 import { AppButton } from '../../../components/Button';
+import { useLogin } from '../../Login/hooks';
+import { LoadingSpinner } from '../../../components';
 
 export const WelcomePage = () => {
   const navigation = useNavigation<RootStackNavigatorProps>();
   const { formData, onEmailChange, onPasswordChange, isPasswordHidden, onPasswordHiddenChange, onLoginSuccess } =
     useForm();
+  const { apiResult, requestApi } = useLogin();
+
+  useEffect(() => {
+    if (apiResult.status === 'success') {
+      onLoginSuccess(apiResult.data, navigation);
+    }
+
+    // if (apiResult.status === 'fail') {
+    //   setSnackBar({
+    //     isVisible: true,
+    //     status: 'error',
+    //     message: 'OOPS. Something went wrong with your email or password.',
+    //     duration: 5000,
+    //   });
+    // }
+  }, [apiResult]);
+
+  const onLoginPress = () => {
+    requestApi({ email: formData.email.value, password: formData.password.value });
+  };
 
   const onForgotMyPasswordPress = useCallback(() => {
     navigation.navigate('ForgotPasswordStackNavigator');
@@ -76,6 +98,7 @@ export const WelcomePage = () => {
               marginBottom: 15,
             }}
             disabled={formData.email.isValidated && formData.password.isValidated ? false : true}
+            onPress={onLoginPress}
           >
             <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf: 'center' }}>
               <VectorIcon.II
@@ -106,6 +129,7 @@ export const WelcomePage = () => {
           </View>
         </View>
       </View>
+      <LoadingSpinner isVisible={apiResult.status === 'loading'} message='Processing now...' textColor='white' />
     </SafeAreaView>
   );
 };
