87b13582bd6ae351e5edc63eb87b7007a05c886e
kojima yosuke <yabbee0828@gmail.com>
2024-08-27 16:49:15 +0900

    Rmv atoms folder

--- atoms/CurrentSpaceState.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-import { atomKeys } from '../recoil';
-import { SpaceType } from '../types';
-
-export const currentSpaceState = atom<SpaceType | undefined>({
-  key: 'currentSpace',
-  default: undefined,
-});
--- atoms/MomentLogs.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-import { atomKeys } from '../recoil';
-import { MomentLogsType } from '../types';
-
-export const momentLogsAtom = atom<MomentLogsType>({
-  key: atomKeys.momentLogs,
-  default: {},
-});
