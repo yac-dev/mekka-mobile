895976eb157749016316e5f21926054e7291298a
kojima yosuke <yabbee0828@gmail.com>
2024-12-22 18:15:36 +0900

    fix no internet connection page

--- features/App/components/NoConnection.tsx
+++ features/App/components/NoConnection.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { EmptyView, AppPressable } from '../../../components';
+import { VectorIcon } from '../../../Icons';
 
 export const NoConnection = () => {
   return (
     <EmptyView
-      title='🤨 No internet connection...'
+      icon={<VectorIcon.FT name='wifi-off' size={48} color='white' style={{ marginBottom: 30 }} />}
+      title="You're offline"
+      subtitle='Please check your internet connection and try again.'
       action={
-        <View>
-          <AppPressable style={{ backgroundColor: 'rgb(70,70,70)', padding: 10, borderRadius: 100 }}>
-            <Text style={{ color: 'white' }}>Retry</Text>
-          </AppPressable>
-        </View>
+        <AppPressable style={{ backgroundColor: 'rgb(70,70,70)', padding: 10, borderRadius: 100, width: 100 }}>
+          <Text style={{ color: 'white', textAlign: 'center' }}>Retry</Text>
+        </AppPressable>
       }
     />
   );
