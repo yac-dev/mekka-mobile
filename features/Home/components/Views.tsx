dfedc0d2a5e3a4749bc94f9d24c1949e43ada07c
kojima yosuke <yabbee0828@gmail.com>
2025-01-11 15:56:52 +0900

    fix tab view navigtion

--- features/Home/components/Views.tsx
+++ features/Home/components/Views.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
 import { GridView } from '../../Space/components/GridView';
 import { RegionView } from './RegionView';
@@ -18,16 +18,30 @@ const routes = [
 export const Views = () => {
   const [index, setIndex] = React.useState(0);
 
+  const onTabPress = (item) => {
+    setIndex(item.key);
+  };
+
   return (
-    <TabView
-      lazy
-      // renderTabBar={() => null}
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: Dimensions.get('window').width }}
-      animationEnabled={false}
-    />
+    <View style={{ flex: 1, backgroundColor: 'black' }}>
+      <TabView
+        lazy
+        renderTabBar={() => null}
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: Dimensions.get('window').width }}
+        animationEnabled={false}
+      />
+      <View style={{ position: 'absolute', bottom: 0, right: 0 }}>
+        <TouchableOpacity onPress={() => setIndex(0)}>
+          <Text style={{ color: 'white' }}>Grid</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setIndex(1)}>
+          <Text style={{ color: 'white' }}>Map</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
     // やっぱ、floatingボタン類はここにおくしかないかもな。。。
     // ここにView toggle用のボタンを置いておくのがいいかもね。。。
   );
