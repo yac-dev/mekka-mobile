21295cbde2fdd545076671cf4c457cc31ee9b91a
kojima yosuke <yabbee0828@gmail.com>
2025-03-08 18:10:06 +0900

    Add tab page components

---
+++ features/MyPage/pages/Activities.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Activities = () => {
+  return (
+    <View>
+      <Text>Activities</Text>
+    </View>
+  );
+};
---
+++ features/MyPage/pages/Posts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Posts = () => {
+  return (
+    <View>
+      <Text>Posts</Text>
+    </View>
+  );
+};
