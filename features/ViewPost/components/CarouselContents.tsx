cd57cbfcaef38bb694fb72d12676fc3a5884598a
kojima yosuke <yabbee0828@gmail.com>
2025-04-05 00:39:10 +0900

    Add carousel to post has multiple contents

--- features/ViewPost/components/CarouselContents.tsx
+++ features/ViewPost/components/CarouselContents.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef } from 'react';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
+import Carousel, { ICarouselInstance, Pagination } from 'react-native-reanimated-carousel';
 import { ContentType, PostType } from '../../../types';
 import { VideoPlayer } from '../../../components';
 import { CustomImage } from '../../../components';
 import { ResizeMode } from 'react-native-video';
+import { useSharedValue } from 'react-native-reanimated';
 
 type CarouselContentsProps = {
   post: PostType;
@@ -14,10 +15,18 @@ const width = Dimensions.get('window').width;
 export const CarouselContents: React.FC<CarouselContentsProps> = ({ post }) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const videoRef = useRef(null);
+  const progress = useSharedValue<number>(0);
+  const ref = useRef<ICarouselInstance>(null);
+  const onPressPagination = (index: number) => {
+    ref.current?.scrollTo({
+      count: index - progress.value,
+      animated: true,
+    });
+  };
 
   const renderItem = ({ item, index }: { item: ContentType; index: number }) => {
     return (
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         {item.type === 'photo' ? (
           <CustomImage source={item.data} contentFit='cover' />
         ) : (
@@ -43,26 +52,58 @@ export const CarouselContents: React.FC<CarouselContentsProps> = ({ post }) => {
     >
       {post.contents.length > 1 ? (
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-          <View style={{ position: 'absolute', top: -10 }}>
+          {/* <View style={{ position: 'absolute', top: -10 }}>
             <PaginationLine activeIndex={activeIndex} contents={post.contents} />
-          </View>
+          </View> */}
           <Carousel
-            loop
-            panGestureHandlerProps={{
-              activeOffsetX: [-10, 10],
-            }}
-            modeConfig={{
-              parallaxScrollingOffset: 70,
-              parallaxScrollingScale: 0.85,
-              parallaxAdjacentItemScale: 0.7,
-            }}
-            width={width}
-            height={400}
+            autoPlayInterval={2000}
             data={post.contents}
-            scrollAnimationDuration={200}
-            onSnapToItem={(index) => setActiveIndex(index)}
+            width={width}
+            height={Dimensions.get('window').height}
+            loop={false}
+            pagingEnabled={true}
+            snapEnabled={true}
+            style={{
+              width: width,
+            }}
+            mode='parallax'
+            modeConfig={{
+              parallaxScrollingScale: 0.9,
+              parallaxScrollingOffset: 45,
+            }}
+            onConfigurePanGesture={(g) => {
+              'worklet';
+              // Disable vertical gestures
+              g.activeOffsetY([-999999, 999999]);
+              // Enable horizontal gestures with a small threshold
+              g.activeOffsetX([-20, 20]);
+            }}
+            onProgressChange={progress}
             renderItem={renderItem}
           />
+          <View style={{ position: 'absolute', bottom: 130 }}>
+            <Pagination.Basic
+              progress={progress}
+              data={post.contents}
+              size={8}
+              dotStyle={{
+                borderRadius: 100,
+                backgroundColor: '#262626',
+              }}
+              activeDotStyle={{
+                borderRadius: 100,
+                overflow: 'hidden',
+                backgroundColor: '#f1f1f1',
+              }}
+              containerStyle={[
+                {
+                  gap: 5,
+                },
+              ]}
+              horizontal
+              onPress={onPressPagination}
+            />
+          </View>
         </View>
       ) : (
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
